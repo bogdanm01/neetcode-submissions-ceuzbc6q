cd50dea0d853da140ecebfb2926c3176683e4cd0
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2026-04-08 12:57:45 +0200

    Add: max-consecutive-ones - submission-23

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-23.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let localMax = 0;
+        let max = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] === 1) {
+                localMax++;
+            } else {
+                localMax = 0;
+            }
+        
+            max = Math.max(max, localMax);
+        }
+
+        return max;
+    }
+}
