ae8c3ce13b672184a8d0e310935f01c87222d5a0
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2026-04-09 23:50:31 +0200

    Add: concatenation-of-array - submission-7

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-7.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        let ans = [];
+
+        for (let i = 0; i < nums.length * 2; i++) {
+            ans[i] = nums[i % nums.length]
+        }
+
+        return ans;
+    }
+}
