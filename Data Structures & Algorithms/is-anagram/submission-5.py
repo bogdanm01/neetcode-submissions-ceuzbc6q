c9bd019f44b4f0415116bc4f11bdd67f282672fb
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2026-09-19 11:40:10 -0700

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count_s, count_t = {}, {} 
+        # racecar
+        # carrace
+
+        for i in range(len(s)):
+            if s[i] in count_s:
+                count_s[s[i]]+=1
+            else:
+                count_s[s[i]]=1
+
+            if t[i] in count_t:
+                count_t[t[i]]+=1
+            else:
+                count_t[t[i]]=1
+
+        return count_s == count_t
